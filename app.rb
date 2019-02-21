c134ce959e99120f8b12f8c9871ff93ed0bbbe1f
itggot-saga-losman <38528635+itggot-saga-losman@users.noreply.github.com>
2019-02-21 10:17:41 +0100

    Fixed create user and login

--- app.rb
+++ app.rb
@@ -39,8 +39,33 @@ get('/createUser') do
     slim(:createUser)
 end
 
+post('/created') do
+    db = SQLite3::Database.new("db/Users.db")
+    db.results_as_hash = true
+
+    Users = db.execute("SELECT * FROM users WHERE Username = ?", params['Username'])
+
+    if params['Username'] && params['Password'] 
+        if Users == []
+            newUser = db.execute("INSERT INTO users (Username,Password) VALUES ((?), (?))", params['Username'], params['Password'])
+            redirect('/')
+        else
+            redirect('/createUser')
+        end
+    end
+    
+end
 
 get('/loggedin/profile') do
+    session[:username] = params["username"]
+    session[:password] = params["password"]
 
     slim(:profile)
 end
+
+post('/logout') do
+    session.destroy
+    redirect('/')
+end
+
+
