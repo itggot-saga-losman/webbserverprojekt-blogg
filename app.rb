587a3abff2196cc6d14ea0a4a3c8748177d820d1
itggot-saga-losman <38528635+itggot-saga-losman@users.noreply.github.com>
2019-03-17 22:27:34 +0100

    Created profile pics

--- app.rb
+++ app.rb
@@ -1,7 +1,13 @@
 require 'sinatra'
 require 'slim'
 require 'sqlite3'
+require 'bcrypt'
 enable :sessions
+
+configure do 
+
+    set :db, SQLite3::Database.new("db/Users.db")
+end
 
 get('/') do
 
@@ -10,29 +16,28 @@ end
 
 get('/login') do
 
-
     slim(:login)
 end
 
-# FIXA SÅ ATT MAN INTE KAN GÖRA NÅGOT DÅ ANVÄNDARNAMNET INTE FINNS
 post('/logged') do 
-    db = SQLite3::Database.new("db/Users.db")
+
+    db = settings.db
     db.results_as_hash = true
 
     result = db.execute("SELECT * FROM users WHERE Username = (?)", params['Username'])
+ 
 
-    id = result[0]
-    session[:userId] = id[2]
-
-    if result == nil 
-        redirect('/logged')
+    if result == []
+        redirect('/login')
     end
 
-    if params["Username"] == result[0]["Username"] && params["Password"] == result[0]["Password"]
+    if params["Username"] == result[0]["Username"] && BCrypt::Password.new(result[0]["Password"]) == params['Password']
+        id = result[0]
+        session[:userId] = id[2]
         redirect("/loggedin/profile/#{:userId}")
         
     else
-        redirect('/logged')
+        redirect('/login')
     end
     
 end
@@ -43,16 +48,17 @@ get('/loggedin/profile/:userId') do
     session[:password] = params["password"]
     name = params["username"]
 
-    db = SQLite3::Database.new("db/Users.db")
+    db = settings.db
     db.results_as_hash = true
 
-    posts_classic = db.execute("SELECT * FROM posts WHERE UserId = (?)", session[:userId])
-    posts = posts_classic.reverse
+    posts_unsorted = db.execute("SELECT * FROM posts WHERE UserId = (?)", session[:userId])
+    posts = posts_unsorted.reverse
 
     slim(:profile, locals:{posts:posts})
 end
 
 post('/logout') do
+
     session.destroy
     redirect('/')
 end
@@ -63,14 +69,17 @@ get('/createUser') do
 end
 
 post('/created') do
-    db = SQLite3::Database.new("db/Users.db")
+    db = settings.db
     db.results_as_hash = true
 
+    # Fixa så att man kollar så att email är unikt
     Users = db.execute("SELECT * FROM users WHERE Username = ?", params['Username'])
 
-    if params['Username'] && params['Password'] 
+    if params['Username'] && params['Password'] && params['Email']
         if Users == []
-            newUser = db.execute("INSERT INTO users (Username,Password) VALUES ((?), (?))", params['Username'], params['Password'])
+            password = BCrypt::Password.create(params['Password']) 
+            
+            newUser = db.execute("INSERT INTO users (Username, Password, Email) VALUES ((?), (?), (?))", params['Username'], password, params['Email'])
             redirect('/')
         else
             redirect('/createUser')
@@ -79,12 +88,58 @@ post('/created') do
 end
 
 post('/newPost') do
-    db = SQLite3::Database.new("db/Users.db")
+    db = settings.db
     db.results_as_hash = true
 
     db.execute("INSERT INTO posts (Title, Text, Img, UserId ) VALUES ((?), (?), (?), (?))", params['Title'], params['Text'], params['Img'], session[:userId])
     redirect('/loggedin/profile/:userId')
 end
 
+post('/deletePost/:postId') do
+    db = settings.db
+    db.results_as_hash = true
 
+    db.execute("DELETE FROM posts WHERE Id = (?)", params["postId"])
 
+    redirect('/loggedin/profile/:userId')
+end
+
+post('/editPostForm/:postId') do
+    session["edit"] = params["postId"]
+    redirect('/loggedin/profile/:userId')
+end
+
+post('/editPost/:postId') do
+    db = settings.db
+    db.results_as_hash = true
+
+    db.execute("UPDATE Posts set Title = (?), Text = (?), Img =(?), UserId = (?) where ID = (?)", params['Title'], params['Text'], params['Img'], session[:userId], session["edit"])
+    session["edit"] = nil 
+    redirect('/loggedin/profile/:userId')
+end
+
+post('/searchUsername') do
+    db = settings.db
+    db.results_as_hash = true
+
+    result = db.execute("SELECT UserId FROM Users WHERE Username = (?)", params['Username'])
+
+    if result != []
+        session[:username] = params['Username'].to_s
+        session[:user] = result[0]["UserId"]
+
+        redirect("/loggedin/user/#{:user}")
+    else
+        redirect('/loggedin/profile/:userId')
+    end
+end
+
+get('/loggedin/user/:user') do
+    db = settings.db
+    db.results_as_hash = true
+
+    posts_classic = db.execute("SELECT * FROM posts WHERE UserId = (?)", session[:user])
+    posts = posts_classic.reverse
+
+    slim(:user, locals:{posts:posts})
+end
